4dd5f6da0f74d727e08b8a9a0dd55106aa6f0711
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2025-02-01 02:41:27 +0200

    update mobile hero sction

--- app/_components/BackgroundHeroSection.tsx
+++ app/_components/BackgroundHeroSection.tsx
@@ -7,7 +7,7 @@ function BackgroundHeroSection() {
   const opacity = useTransform(scrollY, [0, 750], [1, 0]);
 
   return (
-    <div className="overflow-hidden max-sm:hidden">
+    <div className="overflow-hidden">
       <motion.div style={{ opacity }} className="lights transition-all">
         <div className="absolute left-[50%] top-[65%] z-[-200] h-[600px] w-[240px] translate-x-[-50%] translate-y-[-50%] rotate-[70deg] blur-[30px] max-sm:h-[200px]">
           <Image src={"/flower3.png"} fill alt="flower" priority />
@@ -26,7 +26,7 @@ function BackgroundHeroSection() {
           }}
         ></div>
         <div
-          className="absolute left-[-58px] top-[642px] z-[-1] h-[291px] w-[291px] opacity-[30%] blur-[90px]"
+          className="absolute left-[-58px] top-[642px] z-[-1] h-[291px] w-[291px] opacity-[30%] blur-[90px] max-sm:hidden"
           style={{
             background: "linear-gradient(to bottom, #4D54E8 0%, #4D54E8 37%  )",
           }}
@@ -39,7 +39,7 @@ function BackgroundHeroSection() {
           }}
         ></div>
 
-        <div className="absolute left-[-52px] top-[153px] z-[-1] h-[326px] w-[326px] bg-[#171817a8] opacity-[80%] blur-[400px] max-sm:w-full"></div>
+        <div className="absolute left-[-52px] top-[153px] z-[-1] h-[326px] w-[326px] bg-[#171817a8] opacity-[80%] blur-[400px] max-sm:hidden max-sm:w-full"></div>
       </motion.div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Ads from "./_components/Ads";
-import ClientsComments from "./sections/ClientsComments";
-import ContactUs from "./sections/ContactUs";
-import HeroSection from "./sections/HeroSection";
-import ExploreAI from "./sections/ExploreAI";
-import Products from "./sections/Products";
-import Blogs from "./sections/Blogs";
-import Services from "./sections/Services";
+import ClientsComments from "./_sections/ClientsComments";
+import ContactUs from "./_sections/ContactUs";
+import HeroSection from "./_sections/HeroSection";
+import ExploreAI from "./_sections/ExploreAI";
+import Products from "./_sections/Products";
+import Blogs from "./_sections/Blogs";
+import Services from "./_sections/Services";
 
 export default function Home() {
   return (
